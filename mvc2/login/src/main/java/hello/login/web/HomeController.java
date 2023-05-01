553e3268d628f87385e79524792f4ce688cffc32
daadaadaah <daadaadaah@gmail.com>
2023-05-01 18:55:33 +0900

    [김영한-MVC2] 스프링에서 제공하는 @SessionAttribute 사용하기

--- mvc2/login/src/main/java/hello/login/web/HomeController.java
+++ mvc2/login/src/main/java/hello/login/web/HomeController.java
@@ -8,9 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @Controller
@@ -26,15 +24,7 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String homeLogin(HttpServletRequest request, Model model) {
-
-        HttpSession session = request.getSession(false);
-
-        if (session == null) {
-            return "home";
-        }
-
-        Member member = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+    public String homeLogin(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member, Model model) {
 
         //세션에 회원 데이터가 없으면 home
         if (member == null) {
