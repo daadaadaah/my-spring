83706dc06de89aba3d28101b273170cea091da88
daadaadaah <daadaadaah@gmail.com>
2023-05-15 01:59:45 +0900

    [김영한-Core 고급편] 스프링 AOP 실전 예제 - 재시도 AOP

---
+++ spring-core-advanced/aop/src/main/java/hello/aop/exam/aop/RetryAspect.java
@@ -0,0 +1,35 @@
+package hello.aop.exam.aop;
+
+import hello.aop.exam.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class RetryAspect {
+
+    /**
+     * @Around 사용한 이유 : 재시도할 때 언제 내가 joinPoint.proceed() 호출할 지를 내가 다시 결정해야 되므로,
+     * @Around("@annotation(retry)") 사용한 이유 : doRetry의 매개변수로 retry를 받으므로,
+     */
+    @Around("@annotation(retry)")
+    public Object doRetry(ProceedingJoinPoint joinPoint, Retry retry) throws Throwable {
+        log.info("[retry] {} retry={}", joinPoint.getSignature(), retry);
+
+        int maxRetry = retry.value();
+
+        Exception exceptionHolder = null;
+
+        for (int retryCount = 1; retryCount <= maxRetry; retryCount++) {
+            try {
+                log.info("[retry] try count={}/{}", retryCount, maxRetry);
+                return joinPoint.proceed();
+            } catch (Exception e) {
+                exceptionHolder = e;
+            }
+        }
+        throw exceptionHolder;
+    }
+}
