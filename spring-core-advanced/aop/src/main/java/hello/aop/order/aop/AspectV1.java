3f7eacd0a8bc0e62db25749143282e509b7c9240
daadaadaah <daadaadaah@gmail.com>
2023-05-14 21:28:39 +0900

    [김영한-Core 고급편] 스프링 AOP 구현2 - 포인트컷 분리
    @Pointcut
    - @Pointcut에 포인트컷 표현식을 사용한다.
    - 메서드 이름와 파라미터를 합쳐서 포인트컷 시그니처(signature)라 한다. 여기서, allOrder()가 포인트컷 시그니처이다.
    - 메서드의 반환 타임은 `void`여야 하고, 코드 내용은 비워둔다.
    - 여기서는, `private`로 선언했지만, 다른 Aspect에서 참고하려면 `public`으로 하면 된다.
    - 하나의 포인트컷 표현식을 여러 어드바이스에서 함께 사용할 수 있는 장점이 있다. (모듈화)

--- spring-core-advanced/aop/src/main/java/hello/aop/order/aop/AspectV1.java
+++ spring-core-advanced/aop/src/main/java/hello/aop/order/aop/AspectV1.java
@@ -4,17 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
 
 @Slf4j
 @Aspect
 public class AspectV1 {
 
-    /**
-     * execution(* hello.aop.order..*(..)) : 포인트 컷
-     * doLog : 어드바이스
-     */
-    @Around("execution(* hello.aop.order..*(..))")
+    // hello.aop.order 패키지와 하위 패키지
+    @Pointcut("execution(* hello.aop.order..*(..))") // @Pointcut 에 포인트컷 표현식을 사용한다.
+    private void allOrder(){} // allOrder() : pointcut signature
+
+    @Around("allOrder()")
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
         log.info("[log] {}", joinPoint.getSignature());
         return joinPoint.proceed();
