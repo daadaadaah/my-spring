036482859a1bb929d92fc9701e95e4a5c2b53e0a
daadaadaah <daadaadaah@gmail.com>
2023-05-15 23:46:15 +0900

    [김영한-Core 고급편] 프록시 패턴 및 데코레이터 패턴 - 캐시 프록시 적용

--- spring-core-advanced/proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ spring-core-advanced/proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,18 @@ public class ProxyPatternTest {
     void noProxyTest() {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
-        client.execute();
-        client.execute();
-        client.execute();
+        client.execute(); // 1초 걸림
+        client.execute(); // 1초 걸림
+        client.execute(); // 1초 걸림
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+        client.execute(); // 1초 걸림
+        client.execute(); // 금방 됨 : 캐시에서 가져오므로,
+        client.execute(); // 금방 됨 : 캐시에서 가져오므로,
     }
 }
