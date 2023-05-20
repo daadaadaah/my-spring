bab9f9cb2dc64453c3c8b6990bd361d50b7a0ad1
daadaadaah <daadaadaah@gmail.com>
2023-05-20 18:30:23 +0900

    [재고 시스템으로 알아보는 동시성 이슈] 문제점
    - 0을 예상했으나 89 등 예상과 다른 결과값이 나온다.
    - 왜냐하면, race condition이 발생했기 때문이다.

--- stock/src/test/java/com/example/stock/service/StockServiceTest.java
+++ stock/src/test/java/com/example/stock/service/StockServiceTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -44,5 +47,32 @@ class StockServiceTest {
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        // CountDownLatch : 다른 스레드에서 수행되는 작업이 완료될 때까지 대기할 수 있도록 도와주는 클래스
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (1 * 100) = 0
+        assertEquals(0L, stock.getQuantity());
     }
 }
