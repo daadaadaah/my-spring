22f4ce63c40e895d7a3a588a1751adb2a40905f2
daadaadaah <daadaadaah@gmail.com>
2023-11-14 15:23:18 +0900

    [1장.오브젝트와 의존관계] 1.2 DAO의 분리 - 1.2.2 커넥션 만들기 추출

--- toby-spring/demo/src/main/java/tobyspring/demo/user/dao/UserDao.java
+++ toby-spring/demo/src/main/java/tobyspring/demo/user/dao/UserDao.java
@@ -10,10 +10,10 @@ import tobyspring.demo.user.domain.User;
 public class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("org.h2.Driver");
+        // 관심 1 : DB와 연결을 위한 커넥션을 어떻게 가져올까라는 관심
+        Connection c = getConnection();
 
-        Connection c = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/toby-spring", "sa", "");
-
+        // 관심 2 : 사용자 등록을 위해 DB에 보낼 SQL 문장을 담을 Statement를 만들고 실행하는 관심
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
 
         ps.setString(1, user.getId());
@@ -22,14 +22,13 @@ public class UserDao {
 
         ps.executeUpdate();
 
+        // 관심 3 : 작업이 끝나면, 사용한 리소스인 Statement와 Connection 오브젝트를 닫아줘서 소중한 공유 리소스를 시스템에 돌려주는 것
         ps.close();
         c.close();
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("org.h2.Driver");
-
-        Connection c = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/toby-spring", "sa", "");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 
@@ -69,5 +68,13 @@ public class UserDao {
         System.out.println(user2.getId() + " 조회 성공");
 
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+
+        Connection c = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/toby-spring", "sa", "");
+
+        return c;
     }
 }
